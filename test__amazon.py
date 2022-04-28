6570caed2e5b524e4e8c99ff91a29a8d47c26a92
mokrane hales <mokranehales2013@gmail.com>
2022-04-28 14:40:11 +0200

    premier commit

--- test__amazon.py
+++ test__amazon.py
@@ -1,7 +1,21 @@
+from page_objects.HomePage import HomePage
+from page_objects.BooksPage import BooksPage
+
 from selenium import webdriver
+
 
 def test_amazon1():
     driver = webdriver.Chrome()
-    driver.get("https://www.amazon.fr/")
+    driver.get("https://www.amazon.fr")
     driver.maximize_window()
     driver.quit()
+
+def test_page_object():
+    driver = webdriver.Chrome()
+    driver.get("https://www.amazon.fr")
+    homePage = HomePage(driver)
+    homePage.closecookiepopup()
+    homePage.openAllMenu()
+    homePage.openBookCategory()
+    homePage.openAllBooks()
+
